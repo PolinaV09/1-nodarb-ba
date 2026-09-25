697ddd03b0f31789a0ffd4f00c11827fbfa8778f
PolinaV09 <polikvoronko@gmail.com>
2026-09-25 12:37:59 +0300

    izpildīts

--- 01_bilesu_cena/main.py
+++ 01_bilesu_cena/main.py
@@ -1,9 +1,19 @@
 print("Biļešu cenas kalkulators")
 
 vecums = int(input("Ievadi savu vecumu: "))
+if vecums < 0:
+    print("Vecums nevar būt negatīvs.")
+elif vecums <= 17:
+    cena = 0
+elif vecums <= 64:
+    cena = 7
+else:
+    cena = 4
+if vecums >= 0:
+    print(f"Biļetes cena ir {cena} EUR.")
 
 # TODO: ar if, elif un else nosaki pareizo cenu
 cena = 0
 
-print(f"Biļetes cena ir {cena} EUR.")
 
+    
